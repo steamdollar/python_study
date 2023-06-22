5a3ef7e9e6d3ee14054efc218f3118b221a1e6d6
steamdollar <loerain111@gmail.com>
2023-06-22 11:49:07 +0900

    model training

--- week6/6_project/main.py
+++ week6/6_project/main.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 # ticker, 기간 지정
 ticker_symbol = "AAPL"
 start_date = '2010-01-01'
-end_date = '2023-06-15'
+end_date = '2023-06-21'
 file_name = "AAPL.csv"
     
 # 이미 데이터가 있는 경우 데이터 다운로드를 실행하지 않음
@@ -68,3 +68,99 @@ for i in range(60, len(train_data)):
     
 # Convert the x_train and y_train to numpy arrays 
 x_train, y_train = np.array(x_train), np.array(y_train)
+
+# pip install tensorflow, keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.models import load_model
+
+model_path = 'saved_model.h5'
+
+# Build the LSTM model
+
+if not os.path.exists(model_path):
+# build initialize neural network
+    model = Sequential()
+
+    # add 각 128, 64개의 뉴런을 가진 lstm layer를 추가
+    # return_sequence를 true로 설정하면 output이 다음 lstm layer로 전달될 수 있다.
+    model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(LSTM(64, return_sequences=False))
+
+    # dense layer를 추가, 첫 번째는 25개 뉴런이, 두 번째는 1개 뉴런이 있다.
+    model.add(Dense(25))
+    model.add(Dense(1))
+
+    # Compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    # LSTM은 시계열 데이터이므로 데이터를 넣을때 3차원 데이터를 넣어줘야 함.
+    # 1. # of samples : 데이터 셋에 포함된 데이터 포인트 수 혹은 시퀀스
+    # 2. time steps : length of the sequence, 예를 들어 다음 날짜의 주가를 예측하기 위해 이전 60일간 데이터를
+    # 사용한다면 60일 넣어주면 된다.
+    # 3. feature : 각 time step을 represent하기 위해 사용되는 attribute의 수. 
+    # 여기서는 stock price만을 사용하므로 1
+
+
+    # 모델을 학습시키기 전에 x_train data의 형태를 바꿔 LSTM layer의 input으로 사용할 수 있도록 한다.
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    # model을 훈련
+    # x_train : input data for training the model (reshaped stock price for the training data)
+
+    # y_train : target data for training (stock prices that we want the model to learn to predict)
+
+    # batch_size : gradient update를 샘플 몇개마다 할것인지를 지정하는 매개변수
+    # 1로 설정했다면 모든 샘플마다 weight을 업데이트해준다.
+    # batch 크기가 작을수록 regularization 효과가 크지만 속도가 느려진다.
+
+    # epochs : 모든 input data (x, y 배열 전부)에 대한 반복 횟수.
+    # 보통 모델은 여러 번의 epoch를 거쳐 학습이 진행된다.
+    # epoch 기간 동안 모델은 예측 오류를 최소화하기 위해 각 weight를 조정한다.
+    # 너무 횟수가 크면 overfit이 일어날 수 있고, vice versa
+
+    model.fit(x_train, y_train, batch_size=1, epochs=5)
+    model.save(model_path)
+else:
+    model = load_model(model_path)
+
+# 학습된 모델을 사용해 test data에 대한 예측을 진행하고, 성능을 확인해보자.
+test_data = scaled_data[training_data_len - 60: , :]
+
+# create dataset 
+x_test = []
+y_test = df['Close'][training_data_len:].values
+
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+
+# convert, reshape data     
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+# 예측 price 값을 생성
+predictions = model.predict(x_test)
+
+# undo scaling
+predictions = scaler.inverse_transform(predictions) 
+
+# get RMSE (root mean square error)
+rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+print("==================")
+print(f'RMSE: {rmse}')
+print("==================")
+
+# Plot/create data for the graph
+train = df[:training_data_len]
+valid = df[training_data_len:].copy()
+valid["Predictions"] = predictions
+
+# 시각화
+plt.figure(figsize=(16, 8))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+plt.savefig("pred.png")
